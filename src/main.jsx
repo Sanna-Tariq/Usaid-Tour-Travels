2d4b81973ed302b234f502c4ce58229ad5c8063d
Sanna Tariq <94275600+Sanna-Tariq@users.noreply.github.com>
2025-08-07 16:04:46 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 
   import 'bootstrap/dist/css/bootstrap.min.css';
 
-   import '././assets/font-awesome-lib/icon/font-awesome.min.css'
-   import '././assets/css/style.css'
-   import '././assets/css/responsive.css'
-   import '././assets/css/slick.min.css'
-   import '././assets/css/aos.css'
+   import '@/assets/font-awesome-lib/icon/font-awesome.min.css'
+   import '@/assets/css/style.css'
+   import '@/assets/css/responsive.css'
+   import '@/assets/css/slick.min.css'
+   import '@/assets/css/aos.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import App from './App.jsx'
