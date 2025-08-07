34d4e502368fb54230cb1af39377054128a49ec9
Sanna Tariq <94275600+Sanna-Tariq@users.noreply.github.com>
2025-08-07 15:43:51 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+  import 'bootstrap/dist/css/bootstrap.min.css';
+
+   import '././assets/font-awesome-lib/icon/font-awesome.min.css'
+   import '././assets/css/style.css'
+   import '././assets/css/responsive.css'
+   import '././assets/css/slick.min.css'
+   import '././assets/css/aos.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
